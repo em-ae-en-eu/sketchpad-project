acc945d430eb7037c0ebbaf7d3d3adb1b0786d31
manu <12345678+username@users.noreply.github.com>
2026-05-26 23:42:39 +0530

    complete Sketchpad project

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let container = document.getElementById("container");
 
+const button = document.getElementById("chng-grid-btn");
+button.addEventListener('click' , () => {
+    container.innerHTML = "";
+    let tempButton = prompt('Enter grid size you want');
+    createGrid(tempButton);
+   
+})
+
 function createGrid(size){
     for(let i = 1; i <= size*size; i++){
         const div = document.createElement("div");
+        div.style.width = (960 / size) + "px";
+        div.style.height = (960 / size) + "px";
         container.appendChild(div);
 
         div.addEventListener('mouseover', () => {
@@ -12,3 +22,4 @@ function createGrid(size){
 }
 
 createGrid(16);
+
